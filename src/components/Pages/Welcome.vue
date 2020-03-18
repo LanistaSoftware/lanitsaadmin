8ab7802b427efe26c440b6867d797ec88383bda0
A.B.B <bayhanbayramoglu@gmail.com>
2020-03-18 21:55:14 +0300

    Login Page eklendi.

---
+++ src/components/Pages/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+     <h2>Lanista Admin Panel</h2>   
+     <p>Hoşgeldiniz.</p>
+    </div>
+</template>
+<style lang="less" scoped>
+   div{
+       text-align: center;
+   } 
+</style>
